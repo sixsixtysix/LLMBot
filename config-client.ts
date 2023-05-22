0f82501eb84ca7674652b6945f3a93400ec556e0
sixsixtysix <bp@sixsixtysix.com>
2023-05-22 16:51:43 -0400

    Update config-client.ts
    Color Change

--- config-client.ts
+++ config-client.ts
@@ -1,7 +1,7 @@
 const client = {
   appName: fillDefault(process.env.appName, "OhMyGPT"),
   appLogo: process.env.appLogo ?? undefined,
-  appThemeColor: fillDefault(process.env.appThemeColor, "#22c55e"),
+  appThemeColor: fillDefault(process.env.appThemeColor, "#2d72ff"),
   appSummary: fillDefault(process.env.appSummary, "Ask me any thing you want."),
   exampleInput: process.env.exampleInput ?? "Ask me any thing.",
 };
